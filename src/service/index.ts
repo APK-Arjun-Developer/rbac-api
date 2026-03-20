1efd771033da712449e9effdf7d59b117849cc1e
APK-Arjun-Developer <apkarjundeveloper@gmail.com>
2026-03-20 05:48:11 +0000

    fix before initilation error

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,5 +1,5 @@
-export * from "./auth.service";
 export * from "./base.service";
-export * from "./user.service";
 export * from "./logger.service";
+export * from "./auth.service";
+export * from "./user.service";
 export * from "./app-error";
